0499c186cad06dc17dfa7de6066e336de0830a7c
jazibsalman1 <jazib1341@gmail.com>
2026-06-06 20:53:53 +0500

    changes

---
+++ main.py
@@ -0,0 +1,23 @@
+﻿from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
+import requests
+
+
+app = FastAPI()
+
+# Static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Templates
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse(request, "index.html", {"request": request})
+
+# Run app
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
